9acc2b8ade214fe417eaea00816e413ce87df3fc
kometvest <kometvest@gmail.com>
2026-06-05 20:00:41 +0900

    Update Company.jsx

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -38,6 +38,13 @@ const LEADERSHIP = [
     image: '/coo.png',
     description: 'Freshman at Harbin Institute of Technology specializing in AI, and Electrical & Information Engineering.',
   },
+  {
+    name: 'Minjae Kim',
+    title: 'CTO',
+    linkedin: 'https://www.linkedin.com/in/rninjae-kim',
+    image: '/minjae.jpeg',
+    description: 'Senior at Seoul Science High School specializing in biology & engineering. President of student community. Korea National Representative, 37th International Biology Olympiad.',
+  },
 ];
 
 const FACULTY = [
@@ -47,13 +54,6 @@ const FACULTY = [
     linkedin: 'https://www.linkedin.com/in/주원-함-0b7905412?',
     image: '/cso.png',
     description: 'Senior at Korea Science Academy of KAIST, specializing in biology. Won 2026 ISEF special prize.',
-  },
-  {
-    name: 'Minjae Kim',
-    title: 'Faculty',
-    linkedin: 'https://www.linkedin.com/in/rninjae-kim',
-    image: '/minjae.jpeg',
-    description: 'Senior at Seoul Science High School specializing in biology & engineering. President of student community. Korea National Representative, 37th International Biology Olympiad.',
   },
 ];
 
